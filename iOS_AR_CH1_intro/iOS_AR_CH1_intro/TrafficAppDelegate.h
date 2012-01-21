4a8cb4a876e37273602a8e191905b1a1bcbd11f4
fillerup <mats.fillerup@gmail.com>
2012-01-21 16:26:00 +0100

    First commit

---
+++ iOS_AR_CH1_intro/iOS_AR_CH1_intro/TrafficAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TrafficAppDelegate.h
+//  iOS_AR_CH1_intro
+//
+//  Created by M Fillerup on 21-01-12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrafficAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS_AR_CH1_intro/iOS_AR_CH1_intro/TrafficViewController.h
@@ -0,0 +1,13 @@
+//
+//  TrafficViewController.h
+//  iOS_AR_CH1_intro
+//
+//  Created by M Fillerup on 21-01-12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrafficViewController : UIViewController
+
+@end
