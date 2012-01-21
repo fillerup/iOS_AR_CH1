7aaa55cdea2e5149fbed716faa14a91ecd0ede43
fillerup <mats.fillerup@gmail.com>
2012-01-21 16:26:12 +0100

    First commit

---
+++ iOS_AR_CH1_intro/iOS_AR_CH1_intro/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS_AR_CH1_intro
+//
+//  Created by M Fillerup on 21-01-12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TrafficAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TrafficAppDelegate class]));
+    }
+}
